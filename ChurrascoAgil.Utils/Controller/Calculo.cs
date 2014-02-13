ddeee57015d65b88e9a393737dfcf54e6ee02011
barcz.gb@gmail.com <barcz.gb@gmail.com@d5deac45-f905-15a2-89bf-0c17276c6de8>
2014-02-13 16:20:27 +0000

    Connection

--- ChurrascoAgil.Utils/Controller/Calculo.cs
+++ ChurrascoAgil.Utils/Controller/Calculo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChurrascoAgil.Utils.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace ChurrascoAgil.Utils.Controller
 {
-    class Calculo
+    public class Calculo
     {
+        public void calcular()
+        {
+            AlimentoDao dao = AlimentoDao.getInstance();
+
+            dao.select("teste");
+
+        }
     }
 }
